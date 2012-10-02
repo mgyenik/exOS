8ea21496f0f1654136ff7201a9dffb14c398a633
Michael Yenik <mgyenik@ncsu.edu>
2012-10-02 15:54:04 -0400

    Context creation working. Now time to schedule!

--- sched.c
+++ sched.c
@@ -8,22 +8,6 @@ extern uint32_t _sheap;
 extern uint32_t _eheap;
 
 #define STACK_SIZE 100
-/*
-tcb current_task = {
-    .stack_top = (uint32_t*)0x10101010,
-    .stack_base = (uint32_t*)0xd00dd00d,
-    .stack_limit = (uint32_t*)0xd00dd00d,
-    .saved_partial_context = {
-        .r4 = 0xd00dd004,
-        .r5 = 0xd00dd005,
-        .r6 = 0xd00dd006,
-        .r7 = 0xd00dd007,
-        .r8 = 0xd00dd008,
-        .r9 = 0xd00dd009,
-        .r10 = 0xd00dd010,
-        .r11 = 0xd00dd011}
-};
-*/
 
 tcb* curr_task = NULL;
 unsigned char switching = 0;
@@ -52,7 +36,7 @@ void save_partial_context(tcb* task) {
 
 void os_start(void) {
     init_region_as_dmem(&_sheap, &_eheap);
-    curr_task = create_context((void(*)(void))main);
+    curr_task = create_context((void(*)(void))(&main));
     init_systick();
     while(1){};    
     //main();
@@ -61,6 +45,7 @@ void os_start(void) {
 void os_tick(void) {
     if(!switching) {
         if(curr_task != NULL) {
+            switching = 1;
             restore_partial_context(curr_task);
             MSP_RESTORE(curr_task)
             asm("bx lr");
@@ -80,14 +65,14 @@ tcb* create_context(void(*func)(void)) {
     new_tcb->stack_base = new_tcb->stack_top;
     new_tcb->stack_limit = new_stack;
     uint32_t reg_default = 0;
-    uint32_t xpsr_default = 0x01000000;
+    uint32_t xpsr_default = 0x21000000;
     *new_tcb->stack_top-- = xpsr_default;
     *new_tcb->stack_top-- = (uint32_t)func;
     *new_tcb->stack_top-- = (uint32_t)func;
     *new_tcb->stack_top-- = (uint32_t)new_tcb->stack_base;
-    *new_tcb->stack_top-- = reg_default;
-    *new_tcb->stack_top-- = reg_default;
-    *new_tcb->stack_top-- = reg_default;
-    *new_tcb->stack_top-- = reg_default;
+    *new_tcb->stack_top-- = reg_default + 3;
+    *new_tcb->stack_top-- = reg_default + 2;
+    *new_tcb->stack_top-- = reg_default + 1;
+    *new_tcb->stack_top = reg_default + 0xa;
     return new_tcb;
 }
