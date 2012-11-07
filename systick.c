49dc829b1db22e8c9108a7dd45264cd8610473c5
Michael Yenik <mgyenik@ncsu.edu>
2012-11-07 18:31:41 -0500

    Extra comment!

--- systick.c
+++ systick.c
@@ -5,5 +5,6 @@ void init_systick(void) {
     /* 250us  at 168Mhz */
     SYSTICK_RELOAD = 16800;
     SYSTICK_VAL = 0;
+    /* Use processor clock, enable interrupt, enable systick peripheral */
     SYSTICK_CTL = 0x00000007;
 }
