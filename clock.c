e88af7b758eb051271a3337c8ba514219691dcf5
Michael Yenik <mgyenik@ncsu.edu>
2013-07-28 01:57:47 -0400

    sam3s: clocks ported

--- clock.c
+++ clock.c
@@ -1,85 +1,65 @@
 #include <kernel.h>
-#include <clock.h>
+#include <reg.h>
 
-extern uint32_t _etext;
-extern uint32_t _sdata;
-extern uint32_t _edata;
-extern uint32_t _sbss;
-extern uint32_t _ebss;
-extern uint32_t _estack;
+typedef struct _ClockConfiguration
+{
 
+    /** Processor clock frequency (in MHz). */
+    uint16_t pck;
+    /** Master clock frequency (in MHz). */
+    uint16_t mck;
+    /** CKGR_PLL reqister value. */
+    uint32_t pllr;
+    /** PMC_MCKR register value. */
+    uint32_t mckr;
+} ClockConfiguration ;
 
-#define HSE_STARTUP_TIMEOUT     (uint16_t) (0x0500)         /* Time out for HSE start up */
-/* PLL Options - See RM0090 Reference Manual pg. 95 */
-#define PLL_M      8            /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_N      336
-#define PLL_P      2            /* SYSCLK = PLL_VCO / PLL_P */
-#define PLL_Q      7            /* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
+/* Clock configurations for the AT91SAM3S4-EK */
+#define CKGR_MUL_SHIFT         16
+#define CKGR_PLLCOUNT_SHIFT     8
+#define CKGR_DIV_SHIFT          0
 
+static const ClockConfiguration clockConfigurations[] = {
+
+    /* PCK = 24 MHz, MCK = 24 MHz
+     * PCK = 12000000 * (7+1) / 2 / 2 = 24 MHz
+     */
+    {24, 24, (CKGR_PLLAR_STUCKTO1 | (7 << CKGR_MUL_SHIFT) \
+        | (0x3f << CKGR_PLLCOUNT_SHIFT) \
+        | (2 << CKGR_DIV_SHIFT)),
+        ( PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK)},
+    /* PCK = 48 MHz, MCK = 48 MHz
+     * PCK = 12000000 * (7+1) / 1 / 2 = 48 MHz
+     */
+    {48, 48, (CKGR_PLLAR_STUCKTO1 | (7 << CKGR_MUL_SHIFT) \
+        | (0x3f << CKGR_PLLCOUNT_SHIFT) \
+        | (1 << CKGR_DIV_SHIFT)),
+        ( PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK)},
+    /* PCK = 64 MHz, MCK = 64 MHz
+     * PCK = 12000000 * (15+1) / 3 / 1 = 64 MHz
+     */
+    {64, 64, (CKGR_PLLAR_STUCKTO1 | (15 << CKGR_MUL_SHIFT) \
+        | (0x3f << CKGR_PLLCOUNT_SHIFT) \
+        | (3 << CKGR_DIV_SHIFT)),
+        ( PMC_MCKR_PRES_CLK | PMC_MCKR_CSS_PLLA_CLK)}
+};
 
 void clocks(void) {
-    /********* Reset clock registers ************/
-    /* Set HSION bit */
-    RCC_CR |= (uint32_t)0x00000001;
-    /* Reset CFGR register */
-    RCC_CFGR = 0x00000000;
-    /* Reset HSEON, CSSON and PLLON bits */
-    RCC_CR &= (uint32_t)0xFEF6FFFF;
-    /* Reset PLLCFGR register */
-    RCC_PLLCFGR = 0x24003010;
-    /* Reset HSEBYP bit */
-    RCC_CR &= (uint32_t)0xFFFBFFFF;
-    /* Disable all interrupts */
-    RCC_CIR = 0x00000000;
-    /******* Set up the clock *************/
-    volatile uint32_t StartUpCounter = 0, HSEStatus = 0;
-    /* Enable HSE */
-    RCC_CR |= RCC_CR_HSEON;
-    /* Wait till HSE is ready and if Time out is reached exit */
-    do {
-        HSEStatus = RCC_CR & RCC_CR_HSERDY;
-        StartUpCounter++;
-    } while((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
-    if ((RCC_CR & RCC_CR_HSERDY) != 0) {
-        HSEStatus = (uint32_t)0x01;
-    }
-    else {
-        HSEStatus = (uint32_t)0x00;
-    }
-    if (HSEStatus == (uint32_t)0x01) {
-        /* Enable high performance mode, System frequency up to 168 MHz */
-        RCC_APB1ENR |= RCC_APB1ENR_PWREN;
-        PWR_CR |= PWR_CR_VOS;  
-        /* HCLK = SYSCLK / 1*/
-        RCC_CFGR |= RCC_CFGR_HPRE_DIV1;
-        /* PCLK2 = HCLK / 2*/
-        RCC_CFGR |= RCC_CFGR_PPRE2_DIV2;
-        /* PCLK1 = HCLK / 4*/
-        RCC_CFGR |= RCC_CFGR_PPRE1_DIV4;
-        /* Configure the main PLL */
-        /* PLL Options - See RM0090 Reference Manual pg. 95 */
-        RCC_PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
-                       (RCC_PLLCFGR_PLLSRC_HSE) | (PLL_Q << 24);
-        /* Enable the main PLL */
-        RCC_CR |= RCC_CR_PLLON;
-        /* Wait till the main PLL is ready */
-        while((RCC_CR & RCC_CR_PLLRDY) == 0) {
-        }
-        /* Configure Flash prefetch, Instruction cache, Data cache and wait state */
-        FLASH_ACR = FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_5WS;
-        /* Select the main PLL as system clock source */
-        RCC_CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-        RCC_CFGR |= RCC_CFGR_SW_PLL;
-        /* Wait till the main PLL is used as system clock source */
-        while ((RCC_CFGR & (uint32_t) RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL); {
-        }
-    }
-    else { 
-        /* If HSE fails to start-up, the application will have wrong clock configuration. */
-        while(1){
-            /* Maybe blink a light o' doom here or something */
-        }
-    }
-    /* Enable the CCM RAM clock */
-    RCC_AHB1ENR |= (1 << 20);
+    int configuration = 2;
+
+    /* Switch to main oscillator in two operations */
+    PMC->PMC_MCKR = (PMC->PMC_MCKR & (uint32_t)~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
+    while ((PMC->PMC_SR & PMC_SR_MCKRDY) == 0);
+
+    /* Configure PLL */
+    PMC->CKGR_PLLAR = clockConfigurations[configuration].pllr;
+    while ((PMC->PMC_SR & PMC_SR_LOCKA) == 0);
+
+    /* Configure master clock in two operations */
+    PMC->PMC_MCKR = (clockConfigurations[configuration].mckr & (uint32_t)~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
+    while ((PMC->PMC_SR & PMC_SR_MCKRDY) == 0);
+    PMC->PMC_MCKR = clockConfigurations[configuration].mckr;
+    while ((PMC->PMC_SR & PMC_SR_MCKRDY) == 0);
+
+    /* DBGU reconfiguration */
 }
